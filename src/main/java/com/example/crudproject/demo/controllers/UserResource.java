d05450bdb156b8a1bf3af68b798eda2275fb4367
fellipetav <fellipe@bytesfritos.com.br>
2024-09-19 18:10:37 -0300

    feat: create interface UserRepository and dependency injection in UserResource

--- src/main/java/com/example/crudproject/demo/controllers/UserResource.java
+++ src/main/java/com/example/crudproject/demo/controllers/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.crudproject.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +8,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 
- * A sample greetings controller to return greeting text
- */
-@RestController
-public class GreetingsController {
+import com.example.crudproject.demo.model.entities.User;
+import com.example.crudproject.demo.model.repository.UserRepository;
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+// Annotation to identify this class as a web resource controlled by a REST controller
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "user/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String grettingText(@PathVariable String name) {
+        User user = new User();
+        user.setName(name);
+
+        userRepository.save(user);
         return "Hello, " + name + "!";
     }
 
---
+++ src/main/java/com/example/crudproject/demo/model/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.crudproject.demo.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.crudproject.demo.model.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
