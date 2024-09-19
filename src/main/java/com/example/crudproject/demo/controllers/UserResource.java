c21e3828132516486a4ff85ba2f425c4796671b0
fellipetav <fellipe@bytesfritos.com.br>
2024-09-19 18:50:15 -0300

    :sparkles: feat: implement enpoint findAll

--- src/main/java/com/example/crudproject/demo/controllers/UserResource.java
+++ src/main/java/com/example/crudproject/demo/controllers/UserResource.java
@@ -1,7 +1,10 @@
 package com.example.crudproject.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.crudproject.demo.model.entities.User;
 import com.example.crudproject.demo.model.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 // Annotation to identify this class as a web resource controlled by a REST controller
 @RestController
@@ -21,11 +28,19 @@ public class UserResource {
     @RequestMapping(value = "user/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String grettingText(@PathVariable String name) {
+        
         User user = new User();
         user.setName(name);
-
         userRepository.save(user);
         return "Hello, " + name + "!";
+    }
+
+    @GetMapping(value = "listAll")
+    @ResponseBody
+    public ResponseEntity<List<User>> listUsers() {
+        
+        List<User> usersList = userRepository.findAll();
+        return new ResponseEntity<List<User>>(usersList, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/crudproject/demo/model/entities/Entity.java
+++
@@ -1,5 +0,0 @@
-package com.example.crudproject.demo.model.entities;
-
-public @interface Entity {
-
-}
