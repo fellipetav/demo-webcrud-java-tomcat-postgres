dbd3a05f1c05a0db5aadb0293bd9bea7ca29ca90
fellipetav <fellipe@bytesfritos.com.br>
2024-09-18 22:21:09 -0300

    :tada: first commit

---
+++ src/main/java/com/example/crudproject/demo/controllers/GreetingsController.java
@@ -0,0 +1,23 @@
+package com.example.crudproject.demo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String grettingText(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
